c57ed8761334c2e1bb9b8ce813a801dedd7a940e
Granite Bagas <granitebagas28@gmail.com>
2021-12-16 21:17:20 +0700

    Create merchant model and migration

---
+++ app/Models/Merchant.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Merchant extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'merchant_name',
+        'created_by',
+        'updated_by'
+    ];
+}
